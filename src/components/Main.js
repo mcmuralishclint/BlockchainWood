a094898fede7704c830f632aca05c2add02b6c49
mcmuralishclint <mcmuralishclint@outlook.com>
2020-12-14 01:27:38 +0530

    loading

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'; 
+
+class Main extends Component {
+
+  render() {
+    return (
+      <div id="content">
+        <p>Welcome your balance is {this.props.ethBalance} ether </p>
+      </div>
+    );
+  }
+}
+
+export default Main;
